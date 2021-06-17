ba24b40c08409ab1169b72321183bfa544727af6
sabarigirishvl <sabarigirishvl@gmail.com>
2021-06-17 07:40:33 +0530

    Included humidity details in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ request({url,json: true},(error,{body}={})=>{
     }
     else{
         callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+
-        ' degrees out. It feels like '+body.current.feelslike+' degrees out.')
+        ' degrees out. It feels like '+body.current.feelslike+' degrees out.The humidity is '+body.current.humidity+'%.')
     }
 })
 
